c826c53ca289abfadd6f5727f0b48dfd9a8a1b55
Jérémy MARMOL <jeremy.marmol@claranet.com>
2025-01-13 12:39:26 +0100

    revert(AZ-1088): revert part of 7e91caad

--- m-logs.tf
+++ m-logs.tf
@@ -2,7 +2,7 @@ module "diagnostic_settings" {
   source  = "claranet/diagnostic-settings/azurerm"
   version = "~> 8.0.0"
 
-  resource_id           = coalesce(one(azurerm_express_route_circuit.main[*].id), var.circuit_id)
+  resource_id           = var.circuit_enabled ? one(azurerm_express_route_circuit.main[*].id) : var.circuit_id
   logs_destinations_ids = var.logs_destinations_ids
   log_categories        = var.logs_categories
   metric_categories     = var.logs_metrics_categories
