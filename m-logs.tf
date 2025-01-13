8fd0b78e797ad0b523769fa6e4a965ab9fb1de04
Jeremy MARMOL <jeremy.marmol@fr.clara.net>
2025-01-13 13:56:25 +0100

    chore(AZ-1088): apply suggestions

--- m-logs.tf
+++ m-logs.tf
@@ -2,7 +2,9 @@ module "diagnostic_settings" {
   source  = "claranet/diagnostic-settings/azurerm"
   version = "~> 8.0.0"
 
-  resource_id           = var.circuit_enabled ? one(azurerm_express_route_circuit.main[*].id) : var.circuit_id
+  count = one(azurerm_express_route_circuit.main[*].id) != null || var.circuit_id != null ? 1 : 0
+
+  resource_id           = coalesce(one(azurerm_express_route_circuit.main[*].id), var.circuit_id)
   logs_destinations_ids = var.logs_destinations_ids
   log_categories        = var.logs_categories
   metric_categories     = var.logs_metrics_categories
@@ -10,4 +12,9 @@ module "diagnostic_settings" {
   custom_name = var.custom_diagnostic_settings_name
   name_prefix = var.name_prefix
   name_suffix = var.name_suffix
+}
+
+moved {
+  from = module.diagnostic_settings
+  to   = module.diagnostic_settings[0]
 }
